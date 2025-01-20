6db590b24337b17470c229dd2886e5472c03c88c
Testriq QA <tech@testriq.com>
2025-01-20 17:45:24 +0530

    Changes
    Updated changes

--- src/test/java/testcases/TC_03_Checkout.java
+++ src/test/java/testcases/TC_03_Checkout.java
@@ -1,9 +1,7 @@
 package testcases;
 
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.IOException;
